d421d7b5a18947b2aeaf6e8f74f8e5b729c63b2c
lpnott <lata.nott@gmail.com>
2018-03-16 13:40:14 -0400

    Update AudioRecorder.js

--- AudioRecorder.js
+++ AudioRecorder.js
@@ -11,8 +11,7 @@ var recorder = new AudioRecorder();
 var startButton = document.getElementById('btn-start-recording');
 var stopButton = document.getElementById('btn-stop-recording');
 
-startButton.onclick = recorder.start;
-stopButton.onclick = recorder.stop;
+
 
 function AudioRecorder(config) {
 
